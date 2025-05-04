e745da9e380d8fe20a0dfee63d9a9193437247f7
Krishna Kumar <kkthecoder@gmail.com>
2025-05-05 01:05:49 +0530

    Common logic for server moved to library

--- apps/jobber-jobs/src/main.ts
+++ apps/jobber-jobs/src/main.ts
@@ -1,25 +1,10 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import * as cookieParser from 'cookie-parser';
-import { ConfigService } from '@nestjs/config';
+import { init } from '@jobber/nestjs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.use(cookieParser());
-  const port = app.get(ConfigService).getOrThrow('PORT');
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  await init(app);
 }
 
 bootstrap();
