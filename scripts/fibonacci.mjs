4f5c12a355af61f4a69048e09d4aa3c1d83d4358
Krishna Kumar <kkthecoder@gmail.com>
2025-05-07 00:59:05 +0530

    Script to generate large topic backlog

---
+++ scripts/fibonacci.mjs
@@ -0,0 +1,70 @@
+const AUTH_API_URL = 'http://localhost:3000/graphql';
+
+const LOGIN_MUTATION = `
+  mutation Login($loginInput: LoginInput!) {
+    login(loginInput: $loginInput) {
+      id
+    }
+  }
+`;
+
+const JOBS_API_URL = 'http://localhost:3001/graphql';
+
+const EXECUTE_JOB_MUTATION = `
+  mutation ExecuteJob($executeJobInput: ExecuteJobInput!) {
+    executeJob(executeJobInput: $executeJobInput) {
+      name
+    }
+  }
+`;
+
+async function login(email, password) {
+  const response = await fetch(AUTH_API_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      query: LOGIN_MUTATION,
+      variables: { loginInput: { email, password } },
+    }),
+  });
+
+  const data = await response.json();
+  const cookies = response.headers.get('set-cookie');
+
+  return { data, cookies };
+}
+
+async function executeJob(executeJobInput, cookies) {
+  const response = await fetch(JOBS_API_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json', Cookie: cookies },
+    body: JSON.stringify({
+      query: EXECUTE_JOB_MUTATION,
+      variables: { executeJobInput },
+    }),
+  });
+
+  const data = await response.json();
+  return data;
+}
+
+(async () => {
+  const { data: loginData, cookies } = await login(
+    'krishnuz@gmail.com',
+    'Krishna@123'
+  );
+  if (loginData?.data.login.id) {
+    const n = 1000;
+    console.log(`Executing fibonacci with n = ${n}`);
+    const executeJobInput = {
+      name: 'Fibonacci',
+      data: Array.from({ length: n }, () => ({
+        iterations: Math.floor(Math.random() * 5000) + 1,
+      })),
+    };
+    const data = await executeJob(executeJobInput, cookies);
+    console.log(data);
+  } else {
+    console.log('Login failed');
+  }
+})();
