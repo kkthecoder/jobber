a84b1dca8f2c65dc15d4c1573a6561e7bdbd0a11
Krishna Kumar <kkthecoder@gmail.com>
2025-05-06 21:34:49 +0530

    Validating job data input

--- apps/jobber-jobs/src/app/jobs/abstract.job.ts
+++ apps/jobber-jobs/src/app/jobs/abstract.job.ts
@@ -1,16 +1,30 @@
 import { serialize } from '@jobber/nestjs';
 import { PulsarClient } from '@jobber/pulsar';
 import { Producer } from 'pulsar-client';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+import { BadRequestException } from '@nestjs/common';
 
-export abstract class AbstractJob<T> {
+export abstract class AbstractJob<T extends object> {
   private producer: Producer;
+  protected abstract messageClass: new () => T;
 
   constructor(private readonly pulsarClient: PulsarClient) {}
 
   async execute(data: T, job: string) {
+    await this.validateData(data);
     if (!this.producer) {
       this.producer = await this.pulsarClient.createProducer(job);
     }
     await this.producer.send({ data: serialize(data) });
+  }
+
+  private async validateData(data: T) {
+    const errors = await validate(plainToInstance(this.messageClass, data));
+    if (errors.length) {
+      throw new BadRequestException(
+        `Job data is invalid: ${JSON.stringify(errors)}`
+      );
+    }
   }
 }
--- apps/jobber-jobs/src/app/jobs/fibonacci.job.ts
+++ apps/jobber-jobs/src/app/jobs/fibonacci.job.ts
@@ -1,13 +1,15 @@
 import { PulsarClient } from '@jobber/pulsar';
 import { Job } from '../decorators/job.decorator';
 import { AbstractJob } from './abstract.job';
-import { FibonacciData } from './fibonacci-data.interface';
+import { FibonacciData } from './fibonacci-data.message';
 
 @Job({
   name: 'Fibonacci',
   description: 'Generate fibonacci sequence and store in db',
 })
 export class FibonacciJob extends AbstractJob<FibonacciData> {
+  protected messageClass = FibonacciData;
+
   constructor(pulsarClient: PulsarClient) {
     super(pulsarClient);
   }
