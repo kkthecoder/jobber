9fc3d85b6f16a8f1bde28af3bc9ff0cc23d42cad
Krishna Kumar <kkthecoder@gmail.com>
2025-05-18 22:00:22 +0530

    Logging for grpc server, added requestId in graphql logging

--- libs/graphql/src/lib/plugins/gql-logger.plugin.ts
+++ libs/graphql/src/lib/plugins/gql-logger.plugin.ts
@@ -5,6 +5,7 @@ import {
   GraphQLRequestListener,
 } from '@apollo/server';
 import { Logger } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
 
 export class GqlLoggingPlugin implements ApolloServerPlugin {
   private readonly logger = new Logger(GqlLoggingPlugin.name);
@@ -13,7 +14,9 @@ export class GqlLoggingPlugin implements ApolloServerPlugin {
   ): Promise<void | GraphQLRequestListener<BaseContext>> {
     const { request } = requestContext;
     const start = Date.now();
+    const requestId = uuidv4();
     this.logger.log({
+      requestId,
       headers: request.http?.headers,
       query: request.query,
       variables: request.variables,
@@ -23,6 +26,7 @@ export class GqlLoggingPlugin implements ApolloServerPlugin {
       willSendResponse: async (responseContext) => {
         const duration = Date.now() - start;
         this.logger.log({
+          requestId,
           query: request.query,
           statusCode: responseContext.response.http?.status || 200,
           duration: `${duration} ms`,
